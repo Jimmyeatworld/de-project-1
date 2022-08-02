f7bd36cda619346b04c22fefbb9f012c3deb9c97
Jimmyeatworld <43929322+Jimmyeatworld@users.noreply.github.com>
2022-08-02 13:32:30 +0300

    update view.sql
    заменил CREATE на CREATE OR REPLACE

--- views.sql
+++ views.sql
@@ -1,26 +1,26 @@
 -- создаём представления для таблиц из схемы production в схему analysis
 
-CREATE VIEW analysis.orderitems AS (
+CREATE OR REPLACE VIEW analysis.orderitems AS (
     SELECT *
     FROM production.orderitems
 );
-CREATE VIEW analysis.orders AS (
+CREATE OR REPLACE VIEW analysis.orders AS (
     SELECT *
     FROM production.orders
 );
-CREATE VIEW analysis.orderstatuses AS (
+CREATE OR REPLACE VIEW analysis.orderstatuses AS (
     SELECT *
     FROM production.orderstatuses
 );
-CREATE VIEW analysis.orderstatuslog AS (
+CREATE OR REPLACE VIEW analysis.orderstatuslog AS (
     SELECT *
     FROM production.orderstatuslog
 );
-CREATE VIEW analysis.products AS (
+CREATE OR REPLACE VIEW analysis.products AS (
     SELECT *
     FROM production.products
 );
-CREATE VIEW analysis.users AS (
+CREATE OR REPLACE VIEW analysis.users AS (
     SELECT *
     FROM production.users
 );
