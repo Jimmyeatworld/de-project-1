b1d71cb2a494b9cc8fd4949249f375f1b2d84e14
Jimmyeatworld <43929322+Jimmyeatworld@users.noreply.github.com>
2022-08-02 14:44:29 +0300

    update dm_rfm_recency.sql
    добавил в запрос клиентов, у которых нет завершенных заказов,
    а также сделал сегментацию клиентов с помощью NTILE(), вместо CASE

--- dm_rfm_recency.sql
+++ dm_rfm_recency.sql
@@ -1,25 +1,19 @@
 -- ДОБАВЛЯЕМ ДАННЫЕ в таблицу analysis.tmp_rfm_recency
 insert into analysis.tmp_rfm_recency (user_id, recency)
--- ПОЛУЧАЕМ МЕТРИКУ recency
--- получаем дату и время последнего успешного заказа по каждому клиенту
+
+-- ПОЛУЧАЕМ МЕТРИКУ recency 
+
 with last_order as (
-	select user_id, max(order_ts) as last_order_time
-	       --sum(payment) as closed_payment
-	from analysis.orders o 
-	where status = 4     -- выбираем только завершенные заказы
+-- получаем время последнего заказа по каждому клиенту
+	select user_id, max(order_ts) as last_order_time 
+	from analysis.orders 
+	where status = 4     -- выбираем только завершенные заказы 
 	group by user_id
-	),
--- нумеруем и сортируем клиентов по времени последнего заказа
-user_numbers as (
-	select *, 
-	       row_number() over(order by last_order_time) as number
-	from last_order)
--- разбиваем пользователей на сегменты
-select user_id,
-       case when number <= round((select count(1) from user_numbers) / 5.0) then 1
-            when number between round((select count(1) from user_numbers) / 5.0) + 1 and round((select count(1) from user_numbers) / 5.0) * 2 then 2
-            when number between round((select count(1) from user_numbers) / 5.0) * 2 + 1 and round((select count(1) from user_numbers) / 5.0) * 3 then 3
-            when number between round((select count(1) from user_numbers) / 5.0) * 3 + 1 and round((select count(1) from user_numbers) / 5.0) * 4 then 4
-            when number >= round((select count(1) from user_numbers) / 5.0) * 4 + 1 then 5
-            end as recency
-from user_numbers
+    UNION                -- добавляем клиентов у которых нет завершенных заказов 
+    select distinct user_id,
+           (select min(order_ts) from analysis.orders ) as last_order_time -- проставляем им минимальное время
+    from analysis.orders 
+    where user_id not in (select distinct user_id from orders o where status = 4)
+)
+select user_id, NTILE(5) OVER(order by last_order_time) as recency
+from last_order
