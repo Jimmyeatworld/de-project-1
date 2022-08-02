ba2d599bdf3477ad4f90a3c7f0adc4e6d1ab28d1
Jimmyeatworld <43929322+Jimmyeatworld@users.noreply.github.com>
2022-08-02 14:45:47 +0300

    update tmp_rfm_frequency.sql
    добавил в запрос клиентов, у которых нет завершенных заказов,
    а также сделал сегментацию клиентов с помощью NTILE(), вместо CASE

--- tmp_rfm_frequency.sql
+++ tmp_rfm_frequency.sql
@@ -1,29 +1,22 @@
 -- ДОБАВЛЯЕМ ДАННЫЕ в таблицу analysis.tmp_rfm_frequency
 insert into analysis.tmp_rfm_frequency (user_id, frequency)
--- ПОЛУЧАЕМ МЕТРИКУ frequency
--- получаем кол-во завершенных заказов по каждому клиенту
-with closed_orders_count as (
-	select user_id, 
-	       count(1) as closed_orders
-	from analysis.orders o 
-	where status = 4        -- выбираем только завершенные заказы
-	group by user_id
-	),
--- нумеруем и сортируем клиентов по кол-ву заказов
-user_numbers as (
-	select *, 
-	       row_number() over(order by closed_orders) as number
-	from closed_orders_count)
--- разбиваем пользователей на сегменты
-select user_id,
-       case when number <= round((select count(1) from user_numbers) / 5.0) then 1
-            when number between round((select count(1) from user_numbers) / 5.0) + 1 and round((select count(1) from user_numbers) / 5.0) * 2 then 2
-            when number between round((select count(1) from user_numbers) / 5.0) * 2 + 1 and round((select count(1) from user_numbers) / 5.0) * 3 then 3
-            when number between round((select count(1) from user_numbers) / 5.0) * 3 + 1 and round((select count(1) from user_numbers) / 5.0) * 4 then 4
-            when number >= round((select count(1) from user_numbers) / 5.0) * 4 + 1 then 5
-            end as frequency
-from user_numbers
-order by number
+-- ПОЛУЧАЕМ МЕТРИКУ frequency 
+
+with orders_count as ( 
+-- получаем кол-во завершенных заказов по каждому клиенту 
+	select user_id,  
+           count(1) as orders 
+    from analysis.orders 
+    where status = 4        -- выбираем только завершенные заказы 
+    group by user_id 
+    UNION                   -- добавляем клиентов у которых нет завершенных заказов 
+    select distinct user_id,
+           0 as orders 
+    from analysis.orders
+    where user_id not in (select distinct user_id from analysis.orders o where status = 4)
+)
+select user_id, NTILE(5) OVER(order by orders) as frequency
+from orders_count
 
 
 
