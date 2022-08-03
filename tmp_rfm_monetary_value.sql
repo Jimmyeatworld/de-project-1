5b9926f56cc7f50a6e21a93e3a5bcfc6fad5d373
Jimmyeatworld <43929322+Jimmyeatworld@users.noreply.github.com>
2022-08-03 12:33:34 +0300

    tmp_rfm_monetary_value.sql
    заменил UNION на LEFT JOIN для повышения производительности

--- tmp_rfm_monetary_value.sql
+++ tmp_rfm_monetary_value.sql
@@ -2,21 +2,29 @@
 insert into analysis.tmp_rfm_monetary_value (user_id, monetary_value)
 -- ПОЛУЧАЕМ МЕТРИКУ monetary_value 
 
-with orders_payment as ( 
--- получаем суммы завершенных заказов по каждому клиенту 
-	select user_id,  
-           sum(payment) as payment 
-    from analysis.orders 
-    where status = 4        -- выбираем только завершенные заказы 
-    group by user_id 
-    UNION                   -- добавляем клиентов у которых нет завершенных заказов 
-    select distinct user_id,
-           0 as payment 
-    from analysis.orders
-    where user_id not in (select distinct user_id from analysis.orders o where status = 4)
+with orders as (
+
+	select u.id as user_id, 
+	       order_id,
+	       COALESCE(order_ts, cast('2021-12-31' as timestamp)) as order_ts, 
+	       -- заменяем null на дату, которая меньше 2022г, просто, чтобы клиенты без завершенных заказов попали в 1 сегмент
+	       COALESCE(payment, 0) as payment 
+	       -- заменяем null на 0, чтобы можно было просуммировать
+	from analysis.users as u
+	left join (select user_id, order_id, order_ts, payment 
+	           from analysis.orders
+	           where status = 4) as o on u.id = o.user_id 
+),
+orders_payment as (
+
+	select user_id, sum(payment) as payment 
+	from orders
+	group by 1
 )
+
 select user_id, NTILE(5) OVER(order by payment) as monetary_value
 from orders_payment
+
 
 
 
